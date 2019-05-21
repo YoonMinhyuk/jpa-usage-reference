5e28db0fa9980f1133b7005d68749e295d17ab78
yoonminhyuk <ymh92730@gmail.com>
2019-05-21 21:58:27 +0900

    [ Add ] 1. * - init commit

---
+++ src/main/java/me/jpa/usageref/UsageRefApplication.java
@@ -0,0 +1,11 @@
+package me.jpa.usageref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageRefApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsageRefApplication.class, args);
+    }
+}
